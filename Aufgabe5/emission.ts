dd2d3b7886a4b2e4bed51e50b20657d51eacf376
Leon G <leon.gobbert@hs-furtwangen.de>
2020-11-18 22:49:39 +0100

    emission js 50% durch

--- Aufgabe5/emission.ts
+++ Aufgabe5/emission.ts
@@ -1,18 +1,59 @@
-// Afrika Werte
-var afrikaEinsWert : number = 1235.5;
-var afrikaEinsEnde : string = "kg CO2.";
+//Werte Afrika
+var afrika2018 : number = 1235.5;
+var afrika2008 = 1028;
+//Werte Südamerika
+var sa2018 : number = 1261.5;
+var sa2008 : number = 1132.6;
+//Werte Europa
+var euro2018 = 4209.3;
+var euro2008 = 4965.7;
+//Werte Nordamerika
+var na2018 = 6035.6;
+var na2008 = 6600.4;
+//Werte Asien
+var asien2018 = 16274.1;
+var asien2008 = 12954.7;
+//Werte Australien
+var austr2018 = 2100.5;
+var austr2008 = 1993;
 
-var afrikaZweiWert : number = 3.98;
-var afrikaZweiEnde : string = "%.";
-
-var afrikaDreiWert : string = "16.79%";
-
-var afrikaVierWert : string = "207.5kg CO2."
+//Berechnungen:
+var emissionenGesamt = afrika2018 + sa2018 + euro2018 + na2018 + asien2018 + austr2018;
+//Afrika
+var prozentAfrikaGes = ((afrika2018 / emissionenGesamt) * 100).toFixed(1); //.toFixed(1) rundet die Zahl auf nur 1 Dezimalen ab.
+var prozentAfrikaVergl = (((afrika2018 / afrika2008) - 1) * 100).toFixed(1);
+var wertAfrikaVergl = (afrika2018 - afrika2008).toFixed(1);
+//Südamerika
+var prozentSaGes = ((sa2018 / emissionenGesamt) * 100).toFixed(1);
+var prozentSaVergl = (((sa2018 / sa2008) - 1) * 100).toFixed(1);
+var wertSaVergl = (sa2018 - sa2008).toFixed(1);
+//Europa
+var prozentEuroGes = ((euro2018 / emissionenGesamt) * 100).toFixed(1);
+var prozentEuroVergl = (((euro2018 / euro2008) - 1) * 100).toFixed(1);
+var wertEuroVergl = (euro2018 - euro2008).toFixed(1);
+//Nordamerika
+var prozentNaGes = ((na2018 / emissionenGesamt) * 100).toFixed(1);
+var prozentNaVergl = (((na2018 / na2008) - 1) * 100).toFixed(1);
+var wertNaVergl = (na2018 - na2008).toFixed(1);
 
 //Konsolenausgabe Afrika:
-console.log('Die Emission von Afrika ist: ' + afrikaEinsWert + afrikaEinsEnde);
-console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit ' + afrikaZweiWert + afrikaZweiEnde);
-console.log('Für Afrika hat sich 2018 die Emission im Vergleich zu 2008 um ' + afrikaDreiWert + ' verändert.');
-console.log('2018 im Vergleich zu 2008 sind das ' + afrikaVierWert);
+console.log('Die Emission von Afrika ist: ' + afrika2018 + 'kg CO2.');
+console.log('Relativ zur Gesamtemission der Welt verursacht Afrika damit ' + prozentAfrikaGes + '%.');
+console.log('Für Afrika hat sich 2018 die Emission im Vergleich zu 2008 um ' + prozentAfrikaVergl + '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das ' + wertAfrikaVergl + 'kg CO2.');
+//Konsolenausgabe Südamerika
+console.log('Die Emission von Südamerika ist: ' + sa2018);
+console.log('Relativ zur Gesamtemission der Welt verursacht Südamerika damit ' + prozentSaGes + '%');
+console.log('Für Südamerika hat sich 2018 die Emission im Vergleich zu 2008 um ' + prozentSaVergl + '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das ' + wertSaVergl + 'kg CO2.'); 
+//Konsolenausgabe Europa
+console.log('Die Emission von Europa ist: ' + euro2018 + 'kg CO2.');
+console.log('Relativ zur Gesamtemission der Welt verursacht Europa damit ' + prozentEuroGes + '%.');
+console.log('Für Europa hat sich 2018 die Emission im Vergleich zu 2008 um ' + prozentEuroVergl + '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das ' + wertEuroVergl + 'kg CO2.');
+//Konsolenausgabe Nordamerika
+console.log('Die Emission von Nordamerika ist: ' + na2018 + 'kg CO2.');
+console.log('Relativ zur Gesamtemission der Welt verursacht Nordamerika damit ' + prozentNaGes + '%.');
+console.log('Für Europa hat sich 2018 die Emission im Vergleich zu 2008 um ' + prozentNaVergl + '% verändert.');
+console.log('2018 im Vergleich zu 2008 sind das ' + wertNaVergl + 'kg CO2.');
 
-//Emission Welt gesamt: 31.116,5kg
